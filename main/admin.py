637961118d56ba0434b913568fe51f5f3349eaae
Renf1l <daniilanf2015@gmail.com>
2026-07-09 01:00:52 +0300

    сделали views/urls/admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage, ProductStock
 
-# Register your models here.
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+class ProductStockInline(admin.StackedInline):
+    model = ProductStock
+    can_delete = False
+    verbose_name_plural = 'Stock'
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'manufacturer', 'stock', 'category', 'price']
+    list_filter = ['category', 'manufacturer']
+    search_fields = ['name', 'manufacturer', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline, ProductStockInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
